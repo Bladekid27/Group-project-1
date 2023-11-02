2bcf9634f31d5b180f16d9dc6ad0249483417353
Bladekid27 <148721801+Bladekid27@users.noreply.github.com>
2023-11-02 13:40:55 -0400

    Update main.cpp
    I've fixed the errors in the program it worked in visual studios

--- main.cpp
+++ main.cpp
@@ -3,35 +3,47 @@
 // any example usage or helpful command line arguments go here
 
 #include <iostream>
+using namespace std;
 // Add any other header files here
 
 ///////////////////////////////////////
 int main()
 {
-score = 0
+    int(score) = 0;
+        cout << " Please Enter a number between 1-10 ", cin >> score;
 
-if (score == 1):
-    cout<< " i" << endl;
-else if (score == 2):
-    cout<< " ii" << endl;
-else if (score == 3):
-    cout<< " iii" << endl;
-else if (score == 4):
-    cout<< " iv" << endl;
-else if (score == 5):
-    cout<< " v" << endl;
-else if (score == 6):
-    cout<< " vi" << endl;
-else if (score == 7):
-    cout<< " vii" << endl;
-else if (score == 8):
-    cout<< " viii" << endl;
-else if (score == 9):
-    cout<< " ix" << endl;
-else if (score == 10):
-    cout<< " x" << endl;
-else : 
-    cout<< "Invalid input" << endl;
+        if (score == 1)
+            cout << "i" << endl;
+            
+        else if (score == 2)
+            cout << "ii" << endl;
+
+        else if (score == 3)
+            cout << "iii" << endl;
+            
+        else if (score == 4) 
+            cout << "iv" << endl;
+            
+        else if (score == 5) 
+            cout << "v" << endl;
+            
+        else if (score == 6) 
+            cout << "vi" << endl;
+            
+        else if (score == 7) 
+            cout << "vii" << endl;
+            
+        else if (score == 8) 
+            cout << "viii" << endl;
+            
+        else if (score == 9) 
+            cout << "ix" << endl;
+            
+        else if (score == 10) 
+            cout << "x" << endl;
+            
+        else 
+            cout << "Invalid input" << endl;
 
 }
 
