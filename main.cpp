6b406ea4bc904c88a92cb1685d4defe13157a54e
Bladekid27 <148721801+Bladekid27@users.noreply.github.com>
2023-11-02 13:20:46 -0400

    Update main.cpp
    I added the Framework I need to test it to see if the program will work. Not sure if one failure will fail the rest

--- main.cpp
+++ main.cpp
@@ -8,7 +8,31 @@
 ///////////////////////////////////////
 int main()
 {
-    // Work here
+score = 0
+
+if (score == 1):
+    cout<< " i" << endl;
+if (score == 2):
+    cout<< " ii" << endl;
+if (score == 3):
+    cout<< " iii" << endl;
+if (score == 4):
+    cout<< " iv" << endl;
+if (score == 5):
+    cout<< " v" << endl;
+if (score == 6):
+    cout<< " vi" << endl;
+if (score == 7):
+    cout<< " vii" << endl;
+if (score == 8):
+    cout<< " viii" << endl;
+if (score == 9):
+    cout<< " ix" << endl;
+if (score == 10):
+    cout<< " x" << endl;
+else : 
+    cout<< "Invalid input" << endl;
+
 }
 
 // Place any unused code here so that the instructor can follow your problem solving thoughts.
